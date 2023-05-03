d9c0783574ea589a98134c4b159b095a6672d8ed
Kabeer Joshi <89746908+kabeerx9@users.noreply.github.com>
2023-05-03 10:14:49 +0530

    Arsh deleted a vim file and added factorial code

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,22 @@
 
 using namespace std;
 
+// program for factorial 
+
+int fact(int x)
+{
+    if(x<=1)
+        return 1;
+
+    return x*fact(x-1);
+}
 int main()
 {
     int x = 10;
-    cout<<x;
+
+    int res = fact(x);
+    cout<<res;
+    
 
     return 0;
 
