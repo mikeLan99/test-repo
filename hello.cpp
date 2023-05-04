762dec1bbf94e02b6ed568ecf985c7c67e8cffca
Kabeer Joshi <89746908+kabeerx9@users.noreply.github.com>
2023-05-04 11:20:09 +0530

    Memoization added in the dynamic programming code

--- hello.cpp
+++ hello.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 // dynamic programming solution 
 
-int solve(int i , vector<int>&nums)
+int solve(int i , vector<int>&nums , vector<int>&dp)
 {
     if(i==nums.size())
         return 0;
 
+    if(dp[i] != -1)
+        return dp[i];
+
     
-    int take = nums[i] + solve(i+1,nums);
+    int take = nums[i] + solve(i+1,nums,dp);
 
-    int notTake = solve(i+1,nums);
+    int notTake = solve(i+1,nums,dp);
 
-    return max(take,notTake);
+    return dp[i] =  max(take,notTake);
+
 }
 
 
 int main()
 {
-    int res  = solve(0,nums);
+    int n = nums.size();
+    vector<int>dp(n);
+    int res  = solve(0,nums,dp);
 
     cout<<res;
 
